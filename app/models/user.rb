8fa1b7889a4beb6e77d63babc34eed60c2624a2f
Jessie-Bootyard <jessie@bootyard.com>
2023-02-03 17:14:02 +0800

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  validates :new_email, uniqueness: true
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+
+  # has_secure_password
+  # attr_accessor :password_digest      
+  has_many :posts, dependent: :delete_all
+  has_many :comments, dependent: :delete_all
+
+
+  def username
+    email.split('@')[0].capitalize
+  end
+
+  
+end
